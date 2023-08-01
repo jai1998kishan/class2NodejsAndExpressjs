711957b60d2fe4bc75ac72f7d39f2e02752685f7
jai1998kishan <jaikishangupta754@gmail.com>
2023-08-01 15:23:52 +0530

    deletingCartAndDisplayingCart

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -15,9 +13,41 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  //if the query is not define than we will redirect to shop page
+  if(!editMode){
+    return res.redirect('/');
+  }
+  const prodId=req.params.productId;
+  Product.findById(prodId, product =>{
+    if(!product){
+      return res.redirect('/');
+    }
+      res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product:product
+    });
+  }); 
+};
+
+exports.postEditProduct = (req,res,next) =>{
+  const prodId = req.body.productId;
+  const updateTitle = req.body.title;
+  const updatePrice = req.body.price;
+  const updateImageUrl = req.body.imageUrl;
+  const updateDesc = req.body.description;
+  const updateProduct = new Product(prodId,updateTitle,updateImageUrl,updateDesc,updatePrice);
+  updateProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -29,3 +59,10 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+
+exports.postDeleteProduct = (req,res,next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+}
--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,11 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+//this router help to router the edit page by taking rpoduct id also
+router.get('/edit-product/:productId',adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product',adminController.postDeleteProduct);
+
 module.exports = router;
